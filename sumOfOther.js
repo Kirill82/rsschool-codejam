5aa35cce0e70b4201aff52ac5954b2c7b287e87c
Kirill Gorbikov <kirillhor@gmail.com>
2018-10-28 15:58:16 +0300

    feat: add function in sumOfOther.js, create package.json, create package-lock.json, create .gitignore

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,3 +1,11 @@
-function sumOfOther() {
-    
+function sumOfOther(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(sum - arr[i]);
+  }
+  return newArr;
 }
